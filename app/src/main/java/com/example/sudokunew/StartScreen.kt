973b67195e624ecfee395301347eba251e3baad3
AndreasY12 <115213306+AndreasY12@users.noreply.github.com>
2024-11-16 20:01:38 +0200

    made StartScreen scrollable to work in landscape mode on phones

--- app/src/main/java/com/example/sudokunew/StartScreen.kt
+++ app/src/main/java/com/example/sudokunew/StartScreen.kt
@@ -15,7 +15,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DarkMode
 import androidx.compose.material.icons.filled.LightMode
@@ -71,7 +73,8 @@ fun StartScreen(
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(24.dp),
+                .padding(24.dp)
+                .verticalScroll(rememberScrollState()),
             verticalArrangement = Arrangement.SpaceBetween,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
